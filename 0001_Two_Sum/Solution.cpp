ca8f66084ca9e39c77fb1947024bf237eae7ac4e
Harshith Kumar H S <aboutharshith17@gmail.com>
2026-09-25 19:04:09 +0530

    Time: 0 ms(100%), Space: 14.85 MB(44.85%)

--- 0001_Two_Sum/Solution.cpp
+++ 0001_Two_Sum/Solution.cpp
@@ -5,5 +5,41 @@ using namespace std;
 
 class Solution {
 public:
-    // Add the required LeetCode method here.
+    vector<int> twoSum(vector<int>& nums, int target) {
+    // unordered_map<int, int>mp;
+    // for(int i=0; i<nums.size(); i++){
+    //     int diff = target - nums[i];
+
+    //     if(mp.find(diff) != mp.end()){
+    //         return {mp[diff] ,i};
+
+    //     }
+    //     mp[nums[i]]=i;
+    // }  
+    // return {};
+//     unordered_map<int, int>mp;
+
+//     for(int i=0; i<nums.size(); i++){
+//         int complement = target - nums[i];
+
+//         if(mp.find(complement) != mp.end()){
+//             return{mp[complement],i};
+//         }
+//         mp[nums[i]]=i;
+//     }
+//     return {};
+//     }
+// };
+  unordered_map<int, int>mp;
+
+  for(int i = 0 ; i < nums.size(); i++){
+    int complement = target - nums[i];
+
+    if(mp.find(complement) != mp.end()){
+        return {mp[complement], i};
+    }
+    mp[nums[i]] = i;
+  }
+  return {};
+  }
 };
