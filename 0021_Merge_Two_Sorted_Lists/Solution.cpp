023ec25e53a676d438ef2362d343eb5b86f3cb0e
Harshith Kumar H S <aboutharshith17@gmail.com>
2026-09-26 19:30:24 +0530

    Time: 0 ms(100%), Space: 19.42 MB(62.29%)

--- 0021_Merge_Two_Sorted_Lists/Solution.cpp
+++ 0021_Merge_Two_Sorted_Lists/Solution.cpp
@@ -3,7 +3,36 @@ using namespace std;
 
 // LeetCode solution
 
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
-    // Add the required LeetCode method here.
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        if (!list1) return list2;
+        if (!list2) return list1;
+
+        if (list1->val < list2->val) {
+            list1->next= mergeTwoLists(list1->next,list2);
+            return list1;
+        } else {
+            list2->next =mergeTwoLists(list1,list2->next);
+            return list2;
+        }
+    }
 };
+        /**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
