e1cf5f2e537331aa9e2c6b97a131c42b6a6743c6
Harshith Kumar H S <aboutharshith17@gmail.com>
2026-09-26 19:34:59 +0530

    Time: 0 ms(100%), Space: 62.82 MB(78.89%)

--- 0011_Container_With_Most_Water/Solution.cpp
+++ 0011_Container_With_Most_Water/Solution.cpp
@@ -5,5 +5,25 @@ using namespace std;
 
 class Solution {
 public:
-    // Add the required LeetCode method here.
+    int maxArea(vector<int>& height) {
+        int left =0 ; 
+        int right= height.size() -1;
+        int maxWater=0;
+
+      while (left <right) {
+            int h =min(height[left],height[right]);
+            int width=right - left;
+            int area=h * width;
+            maxWater=max(maxWater,area);
+
+           
+    if (height[left]< height[right]) {
+             left++;
+            } else{
+                right--;
+            }
+        }
+
+        return maxWater;
+    }
 };
